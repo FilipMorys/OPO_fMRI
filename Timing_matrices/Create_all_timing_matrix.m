86af0e2bfcf51b44361199eea2973a8e2a8b76be
FilipMorys <filip.morys@gmail.com>
2020-06-16 15:44:29 +0200

    v2

--- Timing_matrices/Create_all_timing_matrix.m
+++ Timing_matrices/Create_all_timing_matrix.m
@@ -21,23 +21,23 @@ SCRIPT TO EXTRACT TIMING FROM DESIGN FILES AND ACCUMULATE THEM IN ONE CELL ARRAY
 %% Set up parameters
 
 All_timing={};
-Files={'OPO_fMRI_34_V3.txt','OPO_fMRI_34_V3.txt'}; % All files will be here
-% Dir = '' % Specify base directory where the files are found
+FilesDir = '/Users/filip/Documents/MPI CBS/OLF_fMRI/Design_files/'; % Specify base directory where the files are found, ONLY design files can be there because I am using dir function to list them all
 
 %% Start a general loop for all steps
 
-for i=1:length(Files)
+for i=3:length(dir(FilesDir))
 
 %% 1. Use functions Importdesfile and Importdesfile_text to read in the data
-
-    Timing=Importdesfile(['~/Downloads/',Files{i}]);
-    Text=Importdesfile_text(['~/Downloads/',Files{i}]);
+    
+    Files=dir(FilesDir);
+    Timing=Importdesfile([FilesDir,Files(i).name]);
+    Text=Importdesfile_text([FilesDir,Files(i).name]);
     
 %% 2. Adjust timing matrices to account for trials without VAS
 
     for j=4:6
         for k=1:46
-            if strcmp(Text(2,k),'nein')==1 % if someone responded 'no' to the perception question, we need to move all the cells by one, because it's not accounted for in the des file
+            if strcmp(Text(2,k),'nein')==1 || strcmp(Text(2,k),'miss')==1 % if someone responded 'no' or misses the response to the perception question, we need to move all the cells by one, because it's not accounted for in the des file
                 Timing(j,:)=[Timing(j,1:k-1) NaN Timing(j,k:end-1)]; % construct the whole row anew - take the first part before the 'no' response, then add NaN for where the 'no' response appeared and then paste the rest of the line after that
             end
         end
@@ -45,13 +45,16 @@ for i=1:length(Files)
     
 %% 3. Convert text to numbers and merge with Timing file
 
-    Timing(7,:)=double(categorical(Text(2,:)));
-    Timing(8,:)=double(categorical(Text(1,:)));
+    for l=7:8
+        Timing(l,:)=double(categorical(Text(l-6,:)));
+    end
+    
     Timing=Timing';
+    Timing(46,:)=[];
     
 %% 4. Accumulate arrays in one cell structure
 
-    All_timing{i,1}=Timing;
+    All_timing{i-2,1}=Timing;
     
 end
 
